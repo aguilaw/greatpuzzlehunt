b5e7ad3bd0ad2ef5fa347b3d241d6d8839f26484
Wendy Aguilar <agu.wnd@gmail.com>
2018-01-19 15:03:26 -0800

    reformatted the whole homepage layout

--- client/components/imports/link-button.jsx
+++ client/components/imports/link-button.jsx
@@ -21,7 +21,6 @@ export default class LinkButton extends Component {
         target='_blank'
       />
     }
-
     return (
       <Link to={this.props.to} style={linkStyle}>
         <Button className={this.props.className} size={this.props.size} color={this.props.color} content={this.props.content} icon={this.props.icon}/>
